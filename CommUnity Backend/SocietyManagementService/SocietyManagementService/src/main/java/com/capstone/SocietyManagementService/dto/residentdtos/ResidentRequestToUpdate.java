f4080cb975ec08c7565252abb721f0c1b3ce77d9
aTul07kn <atulnayak7869@gmail.com>
2024-08-30 18:29:15 +0530

    society, events and updates services done

---
+++ CommUnity Backend/SocietyManagementService/SocietyManagementService/src/main/java/com/capstone/SocietyManagementService/dto/residentdtos/ResidentRequestToUpdate.java
@@ -0,0 +1,15 @@
+package com.capstone.SocietyManagementService.dto.residentdtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResidentRequestToUpdate {
+    private String name;
+    private String phoneNo;
+    private String email;
+    private String password;
+}
